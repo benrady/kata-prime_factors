7c6d18ed86f8e0970a8a95fe84eec749854adc89
Ben Rady <benrady@gmail.com>
2009-12-06 14:13:09 -0600

    Moving back to start state

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,10 +1,7 @@
 class PrimeFactors
 
   module Generator
-    def generate(n, factors=[])
-      return factors if n == 1
-      new_factor = (2..n).find {|f| n % f == 0} 
-      generate(n / new_factor, factors + [new_factor])
+    def generate(n)
     end
   end
 
